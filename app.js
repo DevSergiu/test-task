bf22a6ed3ccc5716df20fdcec2245d354d26d14d
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-09-20 18:32:56 +0300

    group by schedule

--- app.js
+++ app.js
@@ -36,11 +36,92 @@ data = [
   }
 ]
 
+
+
 let output = '';
-for (let i = 0; i < data.length; i++) {
+data.forEach(item => {
   output += `<li>
-               <span class="day"> ${data[i].day}: </span>
-               <span class="hours"> ${data[i].from} - ${data[i].to} </span>
-            </li>`;
-}
+              <span class="day"> ${item.day}: </span>
+              <span class="hours"> ${item.from} - ${item.to} </span>
+             </li>`;
+})
 document.getElementById('data').innerHTML = output;
+
+
+
+const groupBySchedule = scheduleArr => {
+  const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+  const groupedDays = []
+
+  for (let i = 0; i < days.length; i++) {
+    const currentDay = days[i];
+    const previousDay = days[i - 1];
+
+    const currentSchedule = scheduleArr.find(schedule => schedule.day === currentDay);
+    const previousSchedule = scheduleArr.find(schedule => schedule.day === previousDay) || {};
+
+    if (currentSchedule.to === previousSchedule.to && currentSchedule.from === previousSchedule.from) {
+      groupedDays[groupedDays.length - 1].push(currentSchedule);
+    } else {
+      groupedDays.push([currentSchedule])
+    }
+  }
+
+  return groupedDays;
+}
+
+let newData = groupBySchedule(data);
+console.log(newData)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// With For Loop
+
+// for (let i = 0; i < data.length; i++) {
+//   const day = data[i].day;
+//   const fromHour = data[i].from;
+//   const toHour = data[i].to;
+
+//   output += `<li>
+//                <span class="day"> ${day}: </span>
+//                <span class="hours"> ${fromHour} - ${toHour} </span>
+//             </li>`;
+// }
+// document.getElementById('data').innerHTML = output;
+
+
+
+////////
+// let groupBy = function (xs, key) {
+//   return xs.reduce(function (rv, x) {
+//     (rv[x[key]] = rv[x[key]] || []).push(x);
+//     return rv;
+//   }, {});
+// };
+// let newData = groupBy(data, 'from');
+// console.log(newData)
