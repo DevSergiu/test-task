8b7f7efbefd8fc5dc5179e21332ecfd279a870b0
Sergiu-19 <42917431+Sergiu-19@users.noreply.github.com>
2019-09-23 11:38:03 +0300

    rewrite solution

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-data = [
+let data = [
   {
     "day": "sat",
     "from": "10:00",
@@ -39,46 +39,88 @@ data = [
 
 
 
-const groupBySchedule = scheduleArr => {
-  const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
 
-  const groupedDays = []
+const [days, times] = data.reduce(
+  (acc, { day, from, to }) => {
 
-  for (let i = 0; i < days.length; i++) {
-    const currentDay = days[i];
-    const previousDay = days[i - 1];
+    acc[0].push(`<span class="day">${day} </span>`)
+    acc[1].push(`: <span class="hours">${from} - ${to}</span><li>`)
+    return acc
+  },
+  [[], []]
+)
 
-    const currentSchedule = scheduleArr.find(schedule => schedule.day === currentDay);
-    const previousSchedule = scheduleArr.find(schedule => schedule.day === previousDay) || {};
+const output = [
+  ...new Set(
+    times.reduce((acc, time) => {
+      const arr = days.filter((day, i) => times[i] === time)
 
-    if (currentSchedule.to === previousSchedule.to && currentSchedule.from === previousSchedule.from) {
-      groupedDays[groupedDays.length - 1].push(currentSchedule);
-    } else {
-      groupedDays.push([currentSchedule])
-    }
-  }
+      acc.push(arr.join('- ') + time)
+      return acc
+    }, [])
+  ),
+]
 
-  return groupedDays;
-}
-
-let newData = groupBySchedule(data);
-
-
-let output = '';
-let groupedData = newData.forEach(item => {
-  item.forEach((item) => {
-
-    output += `<li>
-              <span class="day"> ${(item.day)}: </span>
-              <span class="hours"> ${item.from} - ${item.to} </span>
-             </li>`;
-
-  })
-})
 document.getElementById('data').innerHTML = output;
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const groupBySchedule = scheduleArr => {
+//   const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+
+//   const groupedDays = []
+
+//   for (let i = 0; i < days.length; i++) {
+//     const currentDay = days[i];
+//     const previousDay = days[i - 1];
+
+//     const currentSchedule = scheduleArr.find(schedule => schedule.day === currentDay);
+//     const previousSchedule = scheduleArr.find(schedule => schedule.day === previousDay) || {};
+
+//     if (currentSchedule.to === previousSchedule.to && currentSchedule.from === previousSchedule.from) {
+//       groupedDays[groupedDays.length - 1].push(currentSchedule);
+//     } else {
+//       groupedDays.push([currentSchedule])
+//     }
+//   }
+
+//   return groupedDays;
+// }
+
+// let newData = groupBySchedule(data);
+
+
+
+
+
+
+
+// let output = '';
+// let groupedData = newData.forEach(item => {
+//   item.forEach((item) => {
+
+//     output += `<li>
+//               <span class="day"> ${(item.day)}: </span>
+//               <span class="hours"> ${item.from} - ${item.to} </span>
+//              </li>`;
+
+//   })
+// })
+// document.getElementById('data').innerHTML = output;
 
 
 
